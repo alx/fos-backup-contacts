d88362e3a6451d92c8dee3d4efeb6f3b8585879e
Alexandre Girard <git@alexgirard.com>
2013-06-21 14:15:59 +0200

    Add scard writing patch from Jason Weathersby
    More info https://hacks.mozilla.org/2013/06/updating-and-tweaking-your-firefox-os-developer-preview-phonegeeksphone/

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -10,9 +10,33 @@ define(['jquery', 'sdcard'], function ($) {
     if (contacts.length <= 0)
       return;
 
-    if (SdCard.checkStorageCard()) {
-
-    }
+var vcard = "BEGIN:VCARD"+"\n"+
+"VERSION:4.0"+"\n"+
+"N:Gump;Forrest;;;"+"\n"+
+"FN:Forrest Gump"+"\n"+
+"ORG:Bubba Gump Shrimp Co."+"\n"+
+"TITLE:Shrimp Man"+"\n"+
+"PHOTO;MEDIATYPE=image/gif:http://www.example.com/dir_photos/my_photo.gif"+"\n"+
+"TEL;TYPE=work,voice;VALUE=uri:tel:+1-111-555-1212"+"\n"+
+"TEL;TYPE=home,voice;VALUE=uri:tel:+1-404-555-1212"+"\n"+
+"ADR;TYPE=work;LABEL='100 Waters Edge\nBaytown, LA 30314\nUnited States of America'"+"\n"+
+" :;;100 Waters Edge;Baytown;LA;30314;United States of America"+"\n"+
+"ADR;TYPE=home;LABEL='42 Plantation St.\nBaytown, LA 30314\nUnited States of America'"+"\n"+
+" :;;42 Plantation St.;Baytown;LA;30314;United States of America"+"\n"+
+"EMAIL:forrestgump@example.com"+"\n"+
+"REV:20080424T195243Z"+"\n"+
+"END:VCARD";
+var sdcard = navigator.getDeviceStorage("sdcard");
+var file = new Blob([vcard], {type: "text/plain"});
+var request = sdcard.addNamed(file, "test/my-file.vcf");
+request.onsuccess = function () {
+var name = this.result;
+alert('File "' + name + '" successfully wrote on the sdcard storage area');
+}
+// An error typically occur if a file with the same name already exist
+request.onerror = function () {
+alert('Unable to write the file: ' + this.error.name);
+}
 
   };
 
